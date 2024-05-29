efd4479a720038ba4e41a9f4a004f7b52f36179d
Tian1003 <hapopo35@gmail.com>
2024-05-29 13:27:04 +0800

    建立檔案

---
+++ 0529/0529/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  0529
+//
+//  Created by 天 on 2024/5/29.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ 0529/0529/_529App.swift
@@ -0,0 +1,17 @@
+//
+//  _529App.swift
+//  0529
+//
+//  Created by 天 on 2024/5/29.
+//
+
+import SwiftUI
+
+@main
+struct _529App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
